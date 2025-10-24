6fa361eacd70db8d1d2662b106dbcfeb3cab41b7
Jérémy Somsouk <jeremy.somsouk@happn.fr>
2025-10-24 16:16:35 +0200

    feat: initialize Spring Boot project

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.7"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.jsomsouk.tennis.kata"
+version = "0.0.1-SNAPSHOT"
+description = "Kata demo tennis game"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	// Spring Boot
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.kafka:spring-kafka")
+
+	// Test
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+	testImplementation("org.junit.jupiter:junit-jupiter")
+	testImplementation("org.assertj:assertj-core")
+	testImplementation("org.mockito:mockito-junit-jupiter")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/com/jsomsouk/tennis/kata/GameSetMatch/GameSetMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsomsouk.tennis.kata.GameSetMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSetMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
