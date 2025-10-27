3bb6fcca4fbe2a56878ccac3ea7a2f4493108860
JeremySomsouk <jeremy@somsouk.fr>
2025-10-27 10:25:52 +0100

    doc: add swagger-ui documentation (#6)

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 
 	// Test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
