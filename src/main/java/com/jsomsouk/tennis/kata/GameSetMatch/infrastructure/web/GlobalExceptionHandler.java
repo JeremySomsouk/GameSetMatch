74460d7a6f5db0317c99ddab8d4a20eac5eede8a
Jérémy Somsouk <jeremy.somsouk@happn.fr>
2025-10-27 10:36:34 +0100

    feat: add basic global exception handler

---
+++ src/main/java/com/jsomsouk/tennis/kata/GameSetMatch/infrastructure/web/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.jsomsouk.tennis.kata.GameSetMatch.infrastructure.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException exception) {
+        return ResponseEntity.badRequest().body(Map.of(
+            "error", "BAD_REQUEST",
+            "message", exception.getMessage()
+        ));
+    }
+}
